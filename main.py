92e92e9c81e16cac924db73e9b6cd4c89e5b8f7c
drgnfireyellow <ethan@milbert.dev>
2022-04-08 11:04:29 -0400

    Add svg saving.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import sys
 
 from rich.console import Console
+
+console = Console(record=True)
+file = open(sys.argv[1])
+filecontent = file.read()
+console.print(filecontent)
+console.save_svg(sys.argv[2], title=sys.argv[1])
